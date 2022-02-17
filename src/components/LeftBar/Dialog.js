a8427d95594ffdc153fe263a7ef6a46a31d12616
dp-arhipov <72319745+dp-arhipov@users.noreply.github.com>
2022-02-17 13:12:00 +0300

    feature: add colored icons and symbol to favourites

--- src/components/LeftBar/Dialog.js
+++ src/components/LeftBar/Dialog.js
@@ -9,13 +9,52 @@ import Badge from "@mui/material/Badge";
 import {styled} from '@mui/material/styles';
 import Box from "@mui/material/Box";
 
+function stringToColor(string) {
+    let hash = 0;
+    let i;
+
+    /* eslint-disable no-bitwise */
+    for (i = 0; i < string.length; i += 1) {
+        hash = string.charCodeAt(i) + ((hash << 5) - hash);
+    }
+
+    let color = '#';
+
+    for (i = 0; i < 3; i += 1) {
+        const value = (hash >> (i * 8)) & 0xff;
+        color += `00${value.toString(16)}`.substr(-2);
+    }
+    /* eslint-enable no-bitwise */
+
+    return color;
+}
+
+function stringAvatar(name) {
+
+    if (name) {
+        const sx = {
+            bgcolor: name != 'Избранное' ? stringToColor(name) : '#1976d2',
+        }
+        if (name == 'Избранное') {
+            return {
+                sx,
+                children: '★'
+            }
+        }else return{
+            sx
+        }
+    }
+
+}
+
+
 const Dialog = ({name, caption, unreadMessagesNumber, ...props}) => {
 
     return (
         <ListItem {...props} >
 
             <ListItemAvatar>
-                <Avatar alt={name} src="/static/images/avatar/1.jpg"/>
+                <Avatar {...stringAvatar(name) }/>
             </ListItemAvatar>
 
                 <ListItemText
